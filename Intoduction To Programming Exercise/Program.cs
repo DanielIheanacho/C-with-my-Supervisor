35a0215d74ceac36297818f045d22baf58b17ad3
DanielIheanacho <dihanacho@gmail.com>
2026-08-28 11:34:15 +0100

    All exercices from chapter one of The Fundamentals to C#

---
+++ Intoduction To Programming Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("GoodDay");
+Console.WriteLine("Iheanacho Daniel\n");
+Console.WriteLine("1\n101\n1001");
+
+DateTime currentDateAndTime = DateTime.Now;
+Console.WriteLine(currentDateAndTime);
+
+double squareRoot = Math.Sqrt(12345);
+Console.WriteLine("squareRoot of 12345 is {0}\n", squareRoot);
+
+for(int i = 1; i < 100; i++)
+{
+    int number = (int)Math.Pow(-1,i+1)*(i+1);
+    Console.Write(number+ ", "); 
+}
+
+Console.WriteLine("\n\nWhat is your age ?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("After 10 years you will be {0}", age + 10);
