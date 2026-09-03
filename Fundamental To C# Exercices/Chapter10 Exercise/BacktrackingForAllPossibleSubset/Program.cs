08719e3a0d68b096af324f473749dc85bf8cd3d3
DanielIheanacho <dihanacho@gmail.com>
2026-09-03 14:02:02 +0100

    Included PrintCurrentCombination Method, and corrected name from Bactracking... to Backtracking...

---
+++ Fundamental To C# Exercices/Chapter10 Exercise/BacktrackingForAllPossibleSubset/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string>  set = new List<string>(["a","b","c","d",]);
+
+PrintCurrentCombination(set);
+
+void PrintCurrentCombination<T>(List<T> currentCombination)
+{   Console.Write("{");
+    for(int i = 0; i < currentCombination.Count; i++)
+    {
+        Console.Write(currentCombination[i] as string);
+    }
+    Console.WriteLine("}");
+}
