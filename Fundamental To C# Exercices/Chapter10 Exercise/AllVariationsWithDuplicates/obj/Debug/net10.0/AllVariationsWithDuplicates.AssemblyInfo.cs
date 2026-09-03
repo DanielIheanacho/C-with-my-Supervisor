b7bfa3ec6b781b22fc3d13d4d12131e3f54ad48d
DanielIheanacho <dihanacho@gmail.com>
2026-09-03 20:13:52 +0100

    Ignores Build files again

--- Fundamental To C# Exercices/Chapter10 Exercise/AllVariationsWithDuplicates/obj/Debug/net10.0/AllVariationsWithDuplicates.AssemblyInfo.cs
+++ Fundamental To C# Exercices/Chapter10 Exercise/AllVariationsWithDuplicates/obj/Debug/net10.0/AllVariationsWithDuplicates.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllVariationsWithDuplicates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08719e3a0d68b096af324f473749dc85bf8cd3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9a1baf30fc418c1daf71910a2f240fdca59a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllVariationsWithDuplicates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllVariationsWithDuplicates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
