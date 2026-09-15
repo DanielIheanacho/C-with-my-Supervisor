8c2c11e26534632cc38afe596a88f433b21fe445
DanielIheanacho <dihanacho@gmail.com>
2026-09-15 12:33:36 +0100

    Created classes for school management

---
+++ SchoolManagement/Program.cs
@@ -0,0 +1,53 @@
+﻿
+public class SchoolManagement
+{
+    private class User
+    {
+        int iD;
+        string firstName;
+        string lastName;
+        string email;
+        Role role;
+        string password;
+
+
+        enum Role
+        {
+            admin,
+            teacher,
+            gardian
+        }
+    }
+
+    private class Subject
+    {
+    int iD;
+    string name;
+    Category category;
+    ClassRoom classRoom;
+    bool isCore;
+
+    enum Category
+        {
+            Sci,
+            Art,
+            Gen
+        }
+    }
+
+    private class ClassRoom
+    {
+        int ID;
+        string name;
+        int Capacity;
+    }
+
+    private class Period
+    {
+        int ID;
+        string name;
+        int startTime;
+        int endTime;
+        bool isBreakPeriod;    
+    }
+}
