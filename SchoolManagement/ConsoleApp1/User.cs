58b546a3fe9a1a29acaf202ebdb0302f5d813819
DanielIheanacho <dihanacho@gmail.com>
2026-09-18 19:19:09 +0100

    Included user registration method, as well as user count and school Id properties

--- SchoolManagement/ConsoleApp1/User.cs
+++ SchoolManagement/ConsoleApp1/User.cs
@@ -7,12 +7,14 @@ namespace ConsoleApp1
 {
     public class User
     {
+        public static int userCount = 0;
         public int Id { get; set; }
         public string FirstName {  get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public Roles Role { get; set; }
         public string PassWord { get; set; }
+        public int SchoolId { get; set; }
 
         public void Print()
         {
@@ -21,6 +23,37 @@ namespace ConsoleApp1
             Console.WriteLine("Email: " + this.Email);
             Console.WriteLine("Role: " + this.Role);
             Console.WriteLine("Password: " + this.PassWord);
+        }
+
+        public User()
+        {
+
+        }
+
+        public static User RegisterUser()
+        {
+
+            User user = new();
+
+
+            user.Id = userCount++;
+
+            user.Role = Roles.Default;
+
+            Console.WriteLine("----Fill Form----");
+            Console.WriteLine("FirstName: ");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("LastName: ");
+            user.LastName = Console.ReadLine();
+
+            Console.WriteLine("Email: ");
+            user.Email = Console.ReadLine();
+
+            Console.WriteLine("Password: ");
+            user.PassWord = Console.ReadLine();
+
+            return user;
         }
     }
 }
