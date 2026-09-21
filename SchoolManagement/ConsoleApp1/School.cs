7c82482740ad510bc745f3663c89d99ffb9112cc
DanielIheanacho <dihanacho@gmail.com>
2026-09-21 09:29:08 +0100

    Removed AssignUser method, included Add User to school, and changed list pf users to list of admins

--- SchoolManagement/ConsoleApp1/School.cs
+++ SchoolManagement/ConsoleApp1/School.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp1
         public static List<School> schools = [];
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<User> users = new();
+        public List<Admin> users = new();
         //public List<Student> students = new();
-        //public List<Subject> subjects = new();
+        public List<Subject> subjects = new();
         //public List<ClassRoom> classRooms = new();
 
         public static School RegisterSchool()
@@ -21,28 +21,18 @@ namespace ConsoleApp1
             School school = new();
             Console.WriteLine("SchooL Name: ");
             school.Name = Console.ReadLine();
-            school.Id = School.schoolId;
+            school.Id = School.schoolId++;
+            Admin admin = Admin.Register(school);
+            school.AdduserToSchool(admin);
             schools.Add(school);
             return school;
         }
 
-        //public static void AssignUser(User user)
-        //{
-        //    for (int i = 0; i < schools.Count; i++)
-        //    {
-        //        if (schools[i].Id != user.SchoolId && i < schools.Count - 1)
-        //        {
-        //            continue;
-        //        }
-        //        else if (schools[i].Id == user.SchoolId)
-        //        {
-        //            schools[i].users.Add(user);
-        //            return;
-        //        }
-        //        Console.WriteLine("User was assigned to a school not in database.");
-        //    }
-        //}
 
+        public void AdduserToSchool(User user)
+        {
+            user.SchoolId = this.Id;
+        }
         public void PrintUsers()
         {
             for (int i = 0; i < users.Count; i++)
