a3dffb438ad21cfa7a130a6c016e108d75a1a03c
DanielIheanacho <dihanacho@gmail.com>
2026-09-21 09:26:52 +0100

    Included register subject, and  moditied Register function to accept a School to add admin to

--- SchoolManagement/ConsoleApp1/Admin.cs
+++ SchoolManagement/ConsoleApp1/Admin.cs
@@ -7,12 +7,14 @@ namespace ConsoleApp1
     public class Admin : User
     {
         public string StaffId { get; set; }
+        School School { get; set; }
 
-        public static Admin Register()
+        public static Admin Register(School school)
         {
-            School school = School.RegisterSchool();
-            Console.WriteLine("New School registered.");
+            //this.School = School.RegisterSchool();
+            Console.WriteLine("New School registered.\n");
 
+            Console.WriteLine("Register New Admin\n");
             Admin admin = new()
             {
                 Id = userCount++,
@@ -40,6 +42,7 @@ namespace ConsoleApp1
 
         public void RegisterTeacher()
         {
+            Console.WriteLine("Register New Teacher\n");
             Teacher teacher = new Teacher
             {
                 Id = userCount++,
@@ -47,7 +50,7 @@ namespace ConsoleApp1
                 SchoolId = this.SchoolId
             };
 
-            Console.WriteLine("----Fill Form----");
+            Console.WriteLine("----Fill Form ----");
             Console.WriteLine("FirstName: ");
             teacher.FirstName = Console.ReadLine();
 
@@ -60,6 +63,29 @@ namespace ConsoleApp1
             Console.WriteLine("Password: ");
             teacher.PassWord = Console.ReadLine();
             Console.WriteLine("New Teacher registered for school " + teacher.SchoolId);
+        }
+
+        public void RegisterSubject()
+        {
+            Subject subject = new();
+            Console.WriteLine("----Fill Form ----");
+            Console.WriteLine("Name: ");
+            subject.Name = Console.ReadLine();
+
+            Console.WriteLine("Category: ");
+            subject.Category = Console.ReadLine();
+
+            Console.WriteLine("Is subject core: ");
+            if(String.IsNullOrEmpty(Console.ReadLine()))
+            {
+                subject.IsCore = false;
+            }
+            else
+            {
+                subject.IsCore = true;
+            }
+            this.School.subjects.Add(subject);
+            Console.WriteLine("New subject registered for school " );
 
         }
     }
